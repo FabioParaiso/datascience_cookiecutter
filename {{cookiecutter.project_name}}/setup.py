afec6747d670fbaca4e7e8c7113f35858b8e5832
Fábio Oliveira <fabio.oliveira@mercedes-benz.io>
2021-12-16 18:47:57 +0000

    Updated structure

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -1,40 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 
-
-def readme():
-    """Import the README.md Markdown file and try to convert it to RST format."""
-    try:
-        import pypandoc
-        return pypandoc.convert('README.md', 'rst')
-    except(IOError, ImportError):
-        with open('README.md') as readme_file:
-            return readme_file.read()
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='{{cookiecutter.package_name}}',
     version='{{cookiecutter.package_version}}',
-    description='{{cookiecutter.package_short_description}}',
-    long_description=readme(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-    ],
-    url='{{cookiecutter.project_url}}',
     author='{{cookiecutter.author_name}}',
     author_email='{{cookiecutter.author_email}}',
-    license='{{cookiecutter.package_license}}',
-    packages=['{{cookiecutter.package_name}}'],
+    description='{{cookiecutter.package_short_description}}',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
-        'pypandoc>=1.4',
-        'watermark>=1.8.1',
-        'pandas>=0.24.2',
-        'scikit-learn>=0.20.3',
-        'scipy>=1.2.1',
-        'matplotlib>=3.0.3',
-        'pytest>=4.3.1',
-        'pytest-runner>=4.4',
-        'click>=7.0'
+        'setuptools'
     ],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    packages=[
+        '{{cookiecutter.package_name}}'
+    ],
 )
